ff2c763a57ffc19c034ea468d7e76eaec11970f0
Seeker90 <chris.carver90@icloud.com>
2025-08-21 00:33:26 +0000

    updated src files

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['The dog ','My granma ','His turtle ','My bird ']; 
+  let action = ['ate ','pissed ','crushed ','broked ']; 
+  let what = ['my homework ', 'my phone ', 'the car '];
+  let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+
+  let randomwho = who[Math.floor(Math.random() * who.length)];
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = randomwho + randomaction + randomwhat + randomwhen;
+
+  document.getElementById("excuse").innerText = excuse;
+
+
+
+
 };
